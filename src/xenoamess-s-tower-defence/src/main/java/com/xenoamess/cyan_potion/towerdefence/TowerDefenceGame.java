cb3cfbd5decb4b36ca7afe11489a409e0dccc9e6
XenoAmess <xenoamess@gmail.com>
2026-03-03 13:12:33 +0000

    feat: add xenoamess-s-tower-defence module

---
+++ src/xenoamess-s-tower-defence/src/main/java/com/xenoamess/cyan_potion/towerdefence/TowerDefenceGame.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2020 XenoAmess
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.xenoamess.cyan_potion.towerdefence;
+
+import com.xenoamess.cyan_potion.base.GameManager;
+
+import java.util.Map;
+
+/**
+ * XenoAmess's Tower Defence module entry point.
+ *
+ * @author XenoAmess
+ * @version 0.167.3-SNAPSHOT
+ */
+public class TowerDefenceGame {
+
+    /**
+     * Main entry point for the tower defence game.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        Map<String, String> argsMap = GameManager.generateArgsMap(args);
+        argsMap.put("SettingFilePath", "settings/TowerDefenceSettings.x8l");
+        GameManager gameManager = new GameManager(argsMap);
+        gameManager.startup();
+    }
+}
